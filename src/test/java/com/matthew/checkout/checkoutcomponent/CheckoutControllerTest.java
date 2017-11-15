dfcabb7289a453bfdb944ea6896c87d854bc61cc
MatthewHayworth <kalinowskimateusz94@gmail.com>
2017-11-15 11:32:57 +0100

    updates

--- src/test/java/com/matthew/checkout/checkoutcomponent/CheckoutControllerTest.java
+++ src/test/java/com/matthew/checkout/checkoutcomponent/CheckoutControllerTest.java
@@ -1,7 +1,5 @@
 package com.matthew.checkout.checkoutcomponent;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.matthew.checkout.checkoutcomponent.domain.Item;
 import com.matthew.checkout.checkoutcomponent.domain.PriceCalculator;
 import com.matthew.checkout.checkoutcomponent.service.CheckoutService;
@@ -15,14 +13,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -79,6 +76,20 @@ public class CheckoutControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(org.hamcrest.Matchers.containsString(
                         "{\"totalPrice\":0.0,\"discount\":0.0}")));
+    }
+
+    @Test
+    public void removeItemTest() throws Exception {
+        mvc.perform(delete("/items/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void removeAllItemsTest() throws Exception {
+        mvc.perform(delete("/items")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
